0b5ba40c0c64903738cb6ae5cf22860501176fb1
Riad Nbou <rnbou@e1r4p13.1337.ma>
2021-05-17 19:59:11 +0100

    mabe the lsat tiiiiiiit

--- rick-and-morty/src/App.jsx
+++ rick-and-morty/src/App.jsx
@@ -1,7 +1,7 @@
 import NavBar from './components/navbar/Navbar.jsx'
 import Charachters from './pages/charachters/charachters.jsx'
 import Home from './pages/home/home.jsx'
-import Epesodes from './pages/epesods/epesods.jsx'
+import Epesodes from './pages/epesodes/epesodes.jsx'
 import Locations from './pages/locations/locations.jsx'
 import Favorits from './pages/favorits/favorits.jsx'
 import './App.css';

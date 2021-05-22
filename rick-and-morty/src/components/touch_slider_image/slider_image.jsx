c17681b521c610d58979e62c23180c623b018810
Riad Nbou <rnbou@e1r4p13.1337.ma>
2021-05-22 15:20:22 +0100

    working slide

--- rick-and-morty/src/components/touch_slider_image/slider_image.jsx
+++ rick-and-morty/src/components/touch_slider_image/slider_image.jsx
@@ -1,47 +1,55 @@
 import './slider_image.css'
 import { useState,useEffect } from 'react'
+import {motion} from 'framer-motion'
 
 function SliderImage(){
-	let down = false
-	const width = 1000
-	const height = 500
-	const image_array = ['/slide_images/1.jpg', '/slide_images/2.jpg', '/slide_images/3.jpeg', '/slide_images/4.jpg', '/slide_images/5.jpg', '/slide_images/6.jpeg', '/slide_images/7.jpeg', '/slide_images/8.jpg', '/slide_images/9.jpg', '/slide_images/10.jpg']
-	const [curent,setCurent] = useState(0)
-	const [imageShow, setImageShow] = useState([])
-	const [direction,setDirection] = useEffect(false)
+	const image_array = ['/slide_images/1.jpg', '/slide_images/2.jpg', '/slide_images/3.jpg', '/slide_images/4.jpg', '/slide_images/5.jpg', '/slide_images/6.jpg', '/slide_images/7.jpeg', '/slide_images/8.jpg', '/slide_images/9.jpg', '/slide_images/10.jpg']
+	
 	const len = image_array.length
+	const [curent,setCurent] = useState(0)
+	const [imageShow,setImageShow] = useState([])
+	const [direction,setDirection] = useState(true)
+
 
 	useEffect(()=>{
-		const start = curent - 2  < 0 ? len + (curent - 3) : curent - 2
-		console.log( curent,len,start)
-		setImageShow([...Array(5).keys()].map((i)=> {return( (i+start) % len )}))
-		console.log(imageShow)
-
+		const start = curent - 2 < 0 ? len + curent  -  2 : curent - 2
+		setImageShow([...Array(7).keys()].map((i)=> {return((i+start)%len)}))
 	},[curent])
 
 
 	return(
-		<>
-			<div className="slider-container">
+		<div className="slider-container">
+			<div className="images-slider">
+				{
+					imageShow.map((item,index)=>{
+						return(
+								<motion.div key={item} className="slider-image"
+									initial = {{x: 500 * (index - (direction ? 2 : 5)),
 
-				<div style={direction? `animation:slide-${direction} 0.25s`:''} className="image-slider">
-
-					{
-						imageShow.map((item,index)=>{
-							return(
-								// style={direction? `animation:scale${index}-${direction}`:''}
-								<img  key={image_array[item]} src={image_array[item]} alt={`${item}.img`} className={`slide-image ${index}`} />
+												// scale:
+												
+									}}
+									animate = {{x: 500 * (index - 3)}}
+								>
+									<img src={image_array[item]} alt={index} />
+								</motion.div>
 							)
-						})
-					}
-				</div>
-				<div className="slide-buttons">
-					<div className="slide-button" onClick={()=>{setCurent(curent - 1  < 0 ? len - 1 : curent - 1); setDirection('reverse') }}>{'<'}</div>
-					<div className="slide-button" onClick={()=>{setCurent((curent+1) % len); setDirection(); setDirection('normal')}}>{'>'}</div>
-				</div>
+					})
+
+				}
 
 			</div>
-		</>
+			<div className="buttons-container">
+				<div onClick = {()=>{setCurent(curent - 1 < 0 ? len + curent - 1 : curent - 1); setDirection(false)}} className="left-button">
+
+				</div>
+				<div onClick = {()=>{setCurent((curent + 1) % len); setDirection(true)}} className="right-button">
+
+				</div>
+			</div>
+
+		</div>
+		
 	)
 
 }
